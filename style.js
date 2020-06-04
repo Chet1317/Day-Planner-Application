cbfc733fff28da558118fd5e8cba00d0a9e0a130
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-04 15:49:33 -0400

    changed up styling in order to get planner and calendar to fit properly

--- style.js
+++ style.js
@@ -5,7 +5,7 @@ for(var i=9; i<=17; i++){
     document.querySelector(".container-planner").innerHTML += 
     `<div class = "row">
     <div class = "hour col">${moment(i,"LT").format("hA")}</div>
-<input class = "textearea col-10" id="description"></input>
+<textarea class = "col-10" id="description"></textarea>
 </div>`
 }
 
