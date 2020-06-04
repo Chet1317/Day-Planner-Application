061d6948b6aeb357a9d8542587d9ec309d9d4f19
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-04 14:55:49 -0400

    created calendar to go with daily planner

--- style.js
+++ style.js
@@ -1,2 +1,11 @@
-var dayTime = new Date();
+var dayTime = moment(new Date());
 document.getElementById("currentDay").innerHTML = dayTime
+
+for(var i=9; i<=17; i++){
+    document.querySelector(".container-planner").innerHTML += 
+    `<div class = "row">
+    <div class = "hour col">${moment(i,"LT").format("hA")}</div>
+<input class = "textearea col-10" id="description"></input>
+</div>`
+}
+
