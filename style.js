0b7c579b0e57bad209265128db0acfa791de0b05
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-05 13:28:38 -0400

    resized planner

--- style.js
+++ style.js
@@ -4,7 +4,7 @@ document.getElementById("currentDay").innerHTML = dayTime
 for(var i=9; i<=17; i++){
     document.querySelector(".container-planner").innerHTML += 
     `<div class = "row">
-    <div class = "hour col-2">${moment(i,"LT").format("hA")}</div>
+    <div class = "hour col-3 col-md-2">${moment(i,"LT").format("hA")}</div>
 <textarea class = "col" id="description"></textarea>
 </div>`
 }
